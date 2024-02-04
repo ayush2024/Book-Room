fa9947abb7671165c02e8ae43a87134301a15a84
Ayush Jha <ayushkumarjha666@gmil.com>
2024-02-04 19:45:54 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "thumbs.dreamstime.com",
+      "images.oyoroomscdn.com",
+      "media.istockphoto.com",
+      "i.pinimg.com",
+      "www.symbols.com",
+      "cdn.pixabay.com",
+      "img.freepik.com",
+      "www.livelaw.in",
+      "static.toiimg.com",
+      "encrypted-tbn0.gstatic.com",
+      "assets-prd.raicore.com",
+      "www.compass-pools.co.uk",
+      "ciprianicharlesdesigns.files.wordpress.com",
+      "res.cloudinary.com"
+    ],
+  },
+};
+
+module.exports = nextConfig
---
+++ pages/api/razorpay.js
@@ -0,0 +1,38 @@
+import connectDB from "@/db";
+import Razorpay from "razorpay";
+import shortid from "shortid";
+import Hotel from "@/models/hotel-model";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    connectDB();
+    const razorpay = new Razorpay({
+      key_id: "rzp_test_FzF53gxSJS59cW",
+      key_secret: "BGwnmXniCXQzk4u1094ZUdAi",
+      // key_id: process.env.RAZORPAY_KEY,
+      // key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    const hotel = await Hotel.findById(req.body.id);
+    if (hotel) {
+      const amount = hotel.price;
+      const options = {
+        amount: (amount * 100).toString(),
+        currency: "INR",
+        receipt: shortid.generate(),
+        payment_capture: 1,
+      };
+
+      try {
+        const result = await razorpay.orders.create(options);
+         return res.status(201).json({
+           id: result.id,
+           currency: result.currency,
+           amount: result.amount,
+         });
+      } catch (err) {
+         res.status(400).json(err);
+      }
+    }
+  }
+}
